c1c340a8a8e2901102dbb127f350d776ddbd7cdf
mlozeman-creator <m.lozeman@gmail.com>
2026-04-07 16:48:14 +0200

    Delete api directory

--- api/services/ProductService.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getProducts(){
- return json_decode(file_get_contents(__DIR__.'/../../data/products.json'),true)['products']??[];
-}
-function getProduct($slug){
- foreach(getProducts() as $p){ if($p['slug']==$slug)return $p;}
- return null;
-}

6ca64dd779592174229af102de50bf37dfd9eb66
mlozeman-creator <m.lozeman@gmail.com>
2026-04-07 16:33:51 +0200

    Delete api directory

--- api/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$uri=$_SERVER['REQUEST_URI'];
-if($uri=='/') require 'views/home.php';
-elseif($uri=='/producten') require 'views/producten.php';
-elseif(preg_match('#/product/(.*)#',$uri,$m)){$_GET['slug']=$m[1]; require 'views/product.php';}
-else echo "404";
